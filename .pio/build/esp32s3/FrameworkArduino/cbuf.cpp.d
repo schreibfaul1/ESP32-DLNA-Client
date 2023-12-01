bd48b0e7b64ef515c97b5ff3ae70187a88c5d385
Wolle <schreibfaul1@t-online.de>
2023-12-01 18:46:14 +0100

    Delete .pio/build/esp32s3 directory

--- .pio/build/esp32s3/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32s3/FrameworkArduino/cbuf.cpp.o: \
- /home/wolle/.platformio/packages/framework-arduinoespressif32-src-1bce0ea9e393cde368b46969d581065f/cores/esp32/cbuf.cpp \
- /home/wolle/.platformio/packages/framework-arduinoespressif32-src-1bce0ea9e393cde368b46969d581065f/cores/esp32/cbuf.h
